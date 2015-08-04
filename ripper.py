86d51a5dc3dcfc80f79ad9a3988b7f91c3903cc2
Shawn Wilkinson <me@super3.org>
2015-08-03 23:00:11 -0400

    Base Ripping Script

--- ripper.py
+++ ripper.py
@@ -6,9 +6,10 @@ import threading
 
 # config
 base_url = "http://traffic.sandyspringsga.org/CameraImage.ashx?cameraId={0}"
-camera_list = [16]
+camera_list = [28, 30, 31]
 
 
+# worker function
 def worker(camera_id):
     """thread worker function"""
     loop = True
@@ -17,24 +18,30 @@ def worker(camera_id):
     try:
         # loop
         while loop:
-            print("delay" + str(camera_id) + ":" + str(time.time() - timestamp))
+            # delay since last frame
+            delay = round(time.time() - timestamp, 3)
+            print("Delay on Camera {0} is {1} seconds.".format(camera_id, delay))
             timestamp = time.time()
-            img_file = 'C://CV/cam{0}/{1}.png'.format(str(camera_id), str(timestamp))
 
+            # pull from url
+            print("Requesting {0}".format(base_url.format(camera_id)))
             response = requests.get(base_url.format(camera_id), stream=True)
+
+            # save image
+            img_file = 'C://CV/cam{0}/{1}.png'.format(str(camera_id), str(timestamp))
             with open(img_file, 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
             del response
-
-            print("Requesting " + img_file)
             print("Saved " + img_file + "\n")
 
+            # pause a bit
             time.sleep(0.1)
     except KeyboardInterrupt:
         loop = False
     return
 
 
+# start multi-threading
 threads = []
 for camera in camera_list:
     t = threading.Thread(target=worker, args=(camera,))
