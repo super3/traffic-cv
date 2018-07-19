7c7f5170b101df06bddd3308e96fd1be1648bf8e
Shawn Wilkinson <me@super3.org>
2018-07-19 12:21:08 -0400

    Draw Attempt

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const io = require('socket.io')(3052);
 const Jimp = require('jimp');
 const util = require('util');
+const PImage = require('pureimage');
+const EventEmitter = require('events');
 
 const net = require('./lib/net');
 
@@ -22,6 +24,45 @@ setInterval(async () => {
 			url: `http://traffic.sandyspringsga.gov/CameraImage.ashx?cameraId=${id}`
 		});
 
+		const stream = new EventEmitter();
+
+		const originalPromise = PImage.decodeJPEGFromStream(stream);
+
+		stream.emit('data', res.data);
+		stream.emit('end');
+
+		const original = await originalPromise;
+
+		const ctx = original.getContext('2d');
+
+		ctx.fillStyle = '#00ff00';
+		ctx.beginPath();
+		ctx.arc(50,50,40,0,Math.PI*2,true); // Outer circle
+		ctx.closePath();
+		ctx.fill();
+
+	 	let newImage = Buffer.alloc(0);
+
+		await new Promise(resolve => {
+			const outputStream = {
+				write(buf) {
+					newImage = Buffer.concat([
+						newImage,
+						buf
+					])
+				},
+				end() {
+					resolve(newImage);
+				},
+				on() {
+					
+				}
+			};
+
+			PImage.encodeJPEGToStream(original, outputStream);
+		});
+
+
 		let image = await Jimp.read(res.data);
 		image = await image.crop(122 - 8, 94 - 15, 16, 30);
 
@@ -37,7 +78,7 @@ setInterval(async () => {
 
 		const color = colors[output.indexOf(Math.max(...output))];
 
-		io.emit(`image-original-${id}`, res.data);
+		io.emit(`image-original-${id}`, newImage);
 		io.emit(`image-${id}`, await util.promisify(image.getBuffer.bind(image))('image/jpeg'));
 		io.emit(`color-${id}`, color);
 	}));
