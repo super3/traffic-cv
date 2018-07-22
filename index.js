382f979b87b6d72be9696ba93516407c46117b30
Shawn Wilkinson <me@super3.org>
2018-07-22 16:41:02 -0400

    Clean Up Index.js

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ io.on('connection', socket => {
 	socket.emit('cameras', cameras);
 });
 
+async function cropFromCenter(img, x, y, w, h) {
+	if(!(img instanceof Jimp))
+		img = await Jimp.read(img);
+
+ 	return await img.crop(x - (w/2), y - (h/2), w, h);
+}
+
+async function imageToInput(img) {
+	const binaryImage = await util.promisify(image.getBuffer.bind(image))('image/jpeg');
+	return [...(await Jimp.read(binaryImage)).bitmap.data].map(x => x / 256);
+}
+
+const indexOfMax = arr => arr.indexOf(Math.max(...arr));
+
+function getState(states, output) {
+	return states[indexOfMax(output)];
+}
+
 setInterval(async () => {
 	await Promise.all(cameras.map(async id => {
 		const res = await axios({
@@ -22,21 +40,19 @@ setInterval(async () => {
 			url: `http://traffic.sandyspringsga.gov/CameraImage.ashx?cameraId=${id}`
 		});
 
-		let image = await Jimp.read(res.data);
-		image = await image.crop(122 - 8, 94 - 15, 16, 30);
+		// crop traffic light and pass to neural net
+		image = await cropFromCenter(res.data, 122, 94, 16, 30);
+		const outputs = net.update(await imageToInput(image));
 
-		const lightBuffer = await util.promisify(image.getBuffer.bind(image))('image/jpeg');
-
-		const output = net.update([...(await Jimp.read(lightBuffer)).bitmap.data].map(x => x / 256));
-
+		// map traffic light colors to neural net states
 		const colors = {
 			0: 'Green',
 			1: 'Yellow',
 			2: 'Red'
 		};
+		const color = getState(colors, outputs);
 
-		const color = colors[output.indexOf(Math.max(...output))];
-
+		// send traffic camera image, cropped light, and color to browser
 		io.emit(`image-original-${id}`, res.data);
 		io.emit(`image-${id}`, await util.promisify(image.getBuffer.bind(image))('image/jpeg'));
 		io.emit(`color-${id}`, color);
