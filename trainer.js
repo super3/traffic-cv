d512521a5e0b3637fcfb4b89afbdbc98d0a1be56
Monty Anderson <work@montyanderson.net>
2018-07-23 13:33:46 +0100

    Compatible with multithreaded trainer

--- trainer.js
+++ trainer.js
@@ -1,26 +1,33 @@
 const fs = require('fs');
 const util = require('util');
 const Jimp = require('jimp');
-const PolyNet = require('polynet');
+
+const PolyNet = (() => {
+	try {
+		return require('../polynet');
+	} catch(error) {
+		return require('polynet');
+	}
+})();
 
 const loadImage = require('./lib/loadImage');
 
 (async () => {
 	const images = [
-		...fs.readdirSync(`${__dirname}/images/traning/green`).map(path => ({
+		...fs.readdirSync(`${__dirname}/images/training/green`).map(path => ({
 			path: `training/green/${path}`,
 			outputs: [1, 0, 0]
 		})),
-		...fs.readdirSync(`${__dirname}/images/traning/yellow`).map(path => ({
+		...fs.readdirSync(`${__dirname}/images/training/yellow`).map(path => ({
 			path: `training/yellow/${path}`,
 			outputs: [0, 1, 0]
 		})),
-		...fs.readdirSync(`${__dirname}/images/traning/red`).map(path => ({
+		...fs.readdirSync(`${__dirname}/images/training/red`).map(path => ({
 			path: `training/red/${path}`,
 			outputs: [0, 0, 1]
 		}))
 	];
-	
+
 
 	const trainingSet = await Promise.all(images.map(async obj => {
 		return [
@@ -33,10 +40,20 @@ const loadImage = require('./lib/loadImage');
 
 	net.f = x => x / (1 + Math.abs(x));
 
-	net.train(trainingSet, {
+	const startTime = Date.now();
+
+	const trainingConfig = {
 		iterations: 20,
 		incr: 0.05
-	});
+	};
+
+	if(typeof net.trainThreaded === 'function') {
+		await net.trainThreaded(trainingSet, trainingConfig);
+	} else {
+		net.train(trainingSet, trainingConfig);
+	}
+
+	console.log(`Training took ${Date.now() - startTime}ms.`);
 
 	fs.writeFileSync(`${__dirname}/net.json`, JSON.stringify(net));
 })();
